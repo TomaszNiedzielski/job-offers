56083d637fa2af60a962572cdd88887933c5578c
Tomasz <tomasz9531@gmail.com>
2020-09-28 09:27:35 +0200

    Updates

---
+++ precache-manifest.4f8b9b55c5d5ad3acea65183d2d0634b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962b42069b8d02ff198c2128cd98c943",
+    "url": "/job-offers/index.html"
+  },
+  {
+    "revision": "d7e4083b351fe2518829",
+    "url": "/job-offers/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "39290917f36637c98387",
+    "url": "/job-offers/static/css/main.a5e9fbd9.chunk.css"
+  },
+  {
+    "revision": "d7e4083b351fe2518829",
+    "url": "/job-offers/static/js/2.e16fcc14.chunk.js"
+  },
+  {
+    "revision": "8b922f29b8f0e5cea89c1a529039a7a1",
+    "url": "/job-offers/static/js/2.e16fcc14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39290917f36637c98387",
+    "url": "/job-offers/static/js/main.ab68021e.chunk.js"
+  },
+  {
+    "revision": "12beee46703723007c54",
+    "url": "/job-offers/static/js/runtime-main.b6f9949e.js"
+  }
+]);
